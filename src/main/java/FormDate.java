c374a41e665574dee6da66c864f22017fa8d4c73
Elena Datsenko <Elenasheglova15414@Gmail.com>
2023-03-09 22:06:14 +0300

    README

---
+++ src/main/java/FormDate.java
@@ -0,0 +1,8 @@
+public class FormDate {
+    public int[] month;
+    public int[] day;
+    public int[] year;
+
+}
+
+
---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Post post = new Post();
+
+        post.name = "Иван";
+        post.passport = "4444 № 44444444";
+        post.patronymic = "Иванович";
+        post.phone = "+7 (999)-999-99-99";
+        post.surname = "Иванов";
+        post.subscription = true;
+
+        post.birthday = new FormDate();
+        post.birthday.day = new int[]{13};
+        post.birthday.month = new int[]{6};
+        post.birthday.year = new int[]{1999};
+    }
+}
